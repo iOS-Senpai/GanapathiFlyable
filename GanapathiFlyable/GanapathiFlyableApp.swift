edd51a07356b85c986532e1dd2d57da20bc9fafa
Pradeep Kumar <pradeep.iossenpai@gmail.com>
2026-09-10 20:08:34 +0530

    Initial Commit

---
+++ GanapathiFlyable/GanapathiFlyableApp.swift
@@ -0,0 +1,17 @@
+//
+//  GanapathiFlyableApp.swift
+//  GanapathiFlyable
+//
+//  Created by Pradeep Kumar on 10/09/26.
+//
+
+import SwiftUI
+
+@main
+struct GanapathiFlyableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
